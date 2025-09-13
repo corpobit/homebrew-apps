5742c3ef0e6b107d46fda261bd207aa31ae77470
Razhan Amini <razhanamini3@gmail.com>
2025-09-13 09:52:46 +0330

    Added example formula for homebrew

---
+++ Formula/nexa.rb
@@ -0,0 +1,11 @@
+class Nexa < Formula
+  desc "Nexa App On Homebrew"
+  homepage "https://corpobit.com/nexa"
+  url "https://github.com/corpobit/public-repo/releases/download/v1.2.3/nexa-1.2.3.zip"
+  sha256 "abcd1234ef567890..." 
+  version "1.2.3"
+
+  def install
+    bin.install "nexa"
+  end
+end
