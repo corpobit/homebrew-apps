8d029d77a59768a85ac7ea75caec9db7fee6f2dd
Razhan Amini <razhanamini3@gmail.com>
2025-09-14 08:36:34 +0330

    Added Structure

--- Casks/nexa.rb
+++ Casks/nexa.rb
@@ -1,6 +1,6 @@
 cask "nexa" do
   version "4.0"
-  sha256 "5d4ce63c8e4fc0fd7053e57f4d295cce6e6cc7eaba80d83de530614bd6b7737d"
+  sha256 "0043fbd6777185972b67b9ab6e0bff80fc5a5df5e3b4192d8f6216f305f20e81"
 
   url "https://corpobit-releases.s3.amazonaws.com/macos/4.0/macos-app.zip"
   name "Nexa"
